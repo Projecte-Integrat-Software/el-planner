f7f196133a35c711981ecafa6e36b42dba6d4c0e
PolG <pgabalsa7@alumnes.ub.ed>
2022-03-27 18:32:30 +0200

    US-FE-07-Calendar Fragment Widget. Primer intent extremadament bàsic.

--- app/src/main/java/com/example/our_planner/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/our_planner/ui/calendar/CalendarFragment.java
@@ -10,28 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.our_planner.R;
 import com.example.our_planner.databinding.FragmentCalendarBinding;
 
 public class CalendarFragment extends Fragment {
 
-    private FragmentCalendarBinding binding;
+//    private FragmentCalendarBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CalendarViewModel calendarViewModel =
-                new ViewModelProvider(this).get(CalendarViewModel.class);
-
-        binding = FragmentCalendarBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textCalendar;
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        return view;
+//        CalendarViewModel calendarViewModel =
+//                new ViewModelProvider(this).get(CalendarViewModel.class);
+//
+//        binding = FragmentCalendarBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textCalendar;
+//        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+//        binding = null;
     }
 }
