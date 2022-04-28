c0eb17201083faf2b0c566765fe6ff8eac9bf22e
mikisf <mikisalafranci@gmail.com>
2022-04-28 13:35:53 +0200

    US-BE-11: Forgot Password Email

--- app/src/main/java/com/example/our_planner/ui/user/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/our_planner/ui/user/ForgotPasswordActivity.java
@@ -8,11 +8,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.our_planner.DataBaseAdapter;
 import com.example.our_planner.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity implements DataBaseAdapter.DBInterface {
 
-    private EditText usernameET;
+    private EditText emailET;
     private Button receiveRecoveryEmailB;
     private Button goBackB;
 
@@ -21,19 +22,23 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        usernameET = findViewById(R.id.usernameET);
+        emailET = findViewById(R.id.emailET);
         receiveRecoveryEmailB = findViewById(R.id.receiveRecoveryEmailB);
         goBackB = findViewById(R.id.goBackB);
 
         //TODO: Connect with ViewModel to check if the username exists, and it it does send the
         // recovery email.
         receiveRecoveryEmailB.setOnClickListener(view -> {
-            String username = usernameET.getText().toString();
-            Toast.makeText(getApplicationContext(), "Sending recovery email to: " + username,
-                    Toast.LENGTH_LONG).show();
+            String email = emailET.getText().toString();
+            if (!email.equals("")) DataBaseAdapter.forgotPassword(this,email);
         });
 
         goBackB.setOnClickListener(view -> startActivity(new Intent(
                 ForgotPasswordActivity.this, LoginActivity.class)));
+    }
+
+    @Override
+    public void setToast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
