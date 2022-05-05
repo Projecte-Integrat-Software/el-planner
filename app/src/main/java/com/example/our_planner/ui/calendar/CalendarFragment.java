0a2e81ae93bf5fc607d8335d9e9dd55989aca403
mmartiga209 <mmartiga209@alumnes.ub.edu>
2022-05-05 07:28:05 +0200

    Arreglant últim commit.

--- app/src/main/java/com/example/our_planner/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/our_planner/ui/calendar/CalendarFragment.java
@@ -1,6 +1,9 @@
 package com.example.our_planner.ui.calendar;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,19 +11,31 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.our_planner.R;
+import com.example.our_planner.model.Group;
 import com.example.our_planner.ui.calendar.comments.CommentsActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CalendarFragment extends Fragment {
 
-    Spinner spinner;
-    Button commentEvent;
+    private Spinner spinner;
+    private Button commentEvent;
+    private ImageButton calendarGroups;
+    private ArrayList<Group> groups;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +49,28 @@ public class CalendarFragment extends Fragment {
         spinner.setAdapter(adapter);
 
         commentEvent = view.findViewById(R.id.commentEvent);
+        calendarGroups = view.findViewById(R.id.btnCalendarGroups);
+        calendarGroups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                PopupWindow pw = new PopupWindow(inflater.inflate(R.layout.popup_calendar_groups, null, false), 450, 400, true);
+                //pw.showAtLocation(button, Gravity.CENTER, 0, 0);
+                pw.showAsDropDown(calendarGroups, 0, 0);
+
+
+                RecyclerView recyclerView = pw.getContentView().findViewById(R.id.recyclerViewCalendarGroups);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false));
+
+                HashMap m = new HashMap();
+                groups.add(new Group("", "PIS", "Theory", m, m, m));
+                groups.add(new Group("", "Geometry", "Problems", m, m, m));
+                groups.add(new Group("", "PAE", "Labs", m, m, m));
+
+                AdapterCalendarGroups adapter = new AdapterCalendarGroups(groups);
+                recyclerView.setAdapter(adapter);
+            }
+        });
 
         return view;
     }
