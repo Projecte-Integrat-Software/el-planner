573b84e0f530f04524abfef0bd7dda01d660f593
PolG <pgabalsa7@alumnes.ub.ed>
2022-05-06 12:25:47 +0200

    US-FE-08-Start time picker implementat

--- app/src/main/java/com/example/our_planner/ui/calendar/CreateEventActivity.java
+++ app/src/main/java/com/example/our_planner/ui/calendar/CreateEventActivity.java
@@ -3,6 +3,7 @@ package com.example.our_planner.ui.calendar;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,12 +20,13 @@ import java.time.LocalTime;
 
 public class CreateEventActivity extends AppCompatActivity {
     private EditText eventNameET;
-    private TextView eventDateTV, eventTimeTV, dateTV;
-    private Button selectDateBtn, createBtn;
+    private TextView eventDateTV, eventTimeTV, dateTV, startTimeTV;
+    private Button selectDateBtn, selectStartTimeBtn, createBtn;
     private DatePickerDialog datePickerDialog;
+    private TimePickerDialog startTimePickerDialog;
 
     private LocalDate date;
-    private LocalTime time;
+    private LocalTime startTime;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -32,48 +34,68 @@ public class CreateEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        time = LocalTime.now();
+        date = CalendarUtils.selectedDate;
+        startTime = LocalTime.now();
+        startTime = startTime.minusSeconds(startTime.getSecond());
         initWidgets();
         initDatePicker();
+        initStartTimePicker();
         initListeners();
     }
 
     @SuppressLint("SetTextI18n")
     private void initWidgets() {
         eventNameET = findViewById(R.id.txtEventTitle);
+
         eventDateTV = findViewById(R.id.txtEventStart);
         eventTimeTV = findViewById(R.id.txtEventEnd);
         dateTV = findViewById(R.id.dateTV);
+        startTimeTV = findViewById(R.id.startTimeTV);
+
         selectDateBtn = findViewById(R.id.selectDateBtn);
+        selectStartTimeBtn = findViewById(R.id.selectStartTimeBtn);
         createBtn = findViewById(R.id.btnCreate);
-        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
-        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+
+
         dateTV.setText(CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        startTimeTV.setText(CalendarUtils.formattedTime(startTime));
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(startTime));
     }
 
     private void initDatePicker() {
         DatePickerDialog.OnDateSetListener dateSetListener = (datePicker, year, month, day) -> {
             month = month + 1;
-            String date = makeDateString(day, month, year);
-            dateTV.setText(date);
+            date = LocalDate.of(year, month, day);
+            dateTV.setText(CalendarUtils.formattedDate(date));
         };
 
-        int year = CalendarUtils.selectedDate.getYear();
-        int month = CalendarUtils.selectedDate.getMonthValue();
-        int day = CalendarUtils.selectedDate.getDayOfMonth();
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
 
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
         datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
     }
 
-    private String makeDateString(int day, int month, int year) {
-        date = LocalDate.of(year, month, day);
-        return CalendarUtils.formattedDate(date);
+    private void initStartTimePicker() {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = (timePicker, selectedHour, selectedMinute) -> {
+            startTime = LocalTime.of(selectedHour, selectedMinute);
+            startTimeTV.setText(CalendarUtils.formattedTime(startTime));
+        };
+
+        int hour = startTime.getHour();
+        int minute = startTime.getMinute();
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        startTimePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
     }
 
     private void initListeners() {
         selectDateBtn.setOnClickListener(this::openDatePicker);
+        selectStartTimeBtn.setOnClickListener(this::openStartTimePicker);
         createBtn.setOnClickListener(this::saveEventAction);
     }
 
@@ -81,9 +103,13 @@ public class CreateEventActivity extends AppCompatActivity {
         datePickerDialog.show();
     }
 
+    public void openStartTimePicker(View view) {
+        startTimePickerDialog.show();
+    }
+
     private void saveEventAction(View view) {
         String eventName = eventNameET.getText().toString();
-        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event newEvent = new Event(eventName, date, startTime);
         Event.eventsList.add(newEvent);
         finish();
     }
